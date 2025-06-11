65908b6b6655dfe294537080a99df398880a04c9
SiHoang18 <lesihoang1804@gmail.com>
2025-06-11 12:50:40 +0700

    first commit

--- src/task/TaskUltraSonic.cpp
+++ src/task/TaskUltraSonic.cpp
@@ -1,7 +1,5 @@
 #include "TaskUltraSonic.h"
-// Create ultrasonic sensor instance
 
-// Ultrasonic ultrasonic(GPIO_NUM_8, GPIO_NUM_9);
 void initUltrasonic()
 {
     pinMode(18, OUTPUT); // Trigger pin
@@ -12,26 +10,23 @@ void getValueUltrasonic(void *pvParameters)
 {
     int trigger = 18;
     int echo = 21;
-    // Ultrasonic ultrasonic(trigger, echo);
     while (1)
     {
-        unsigned long duration; // biến đo thời gian
-        int distance;           // biến lưu khoảng cách
+        unsigned long duration; 
+        int distance;           
         
-        /* Phát xung từ chân trig */
-        digitalWrite(trigger,0);   // tắt chân trig
+        digitalWrite(trigger,0);  
         delayMicroseconds(2);
-        digitalWrite(trigger,1);   // phát xung từ chân trig
-        delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+        digitalWrite(trigger,1);   
+        delayMicroseconds(5);   
         digitalWrite(trigger,0);   
         duration = pulseIn(echo,HIGH);  
-        // Tính khoảng cách đến vật.
+       
         distance = int(duration/2/29.412);
-        
-        /* In kết quả ra Serial Monitor */
+
         Serial.print(distance);
         Serial.println("cm");
         publishData("ultrasonic", String(distance));
-        vTaskDelay(4000 / portTICK_PERIOD_MS); // Delay 1 second
+        vTaskDelay(4000 / portTICK_PERIOD_MS);
     }
 }
